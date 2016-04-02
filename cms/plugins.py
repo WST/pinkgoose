04ebffb3a3172f6ba9ca1b336760c415492bb3d5
WST <admin@jsmart.web.id>
2016-04-03 00:33:14 +0300

    Обнаружение uWSGI, отладочный вывод при старте плагинов

--- cms/plugins.py
+++ cms/plugins.py
@@ -3,3 +3,5 @@
 class AbstractCirnoPlugin:
 	def __init__(self, site):
 		self.site = site
+		self.name = type(self).__module__.split('.')[0]
+		print("Initializing plugin: %s" % self.name)
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 from cms import site
+
+using_uwsgi = False
+try:
+	import uwsgi
+	using_uwsgi = True
+	try:
+		import uwsgidecorators
+	except:
+		print("Running under uWSGI, but also need uwsgidecorators module. Exitting.")
+		sys.exit(-1)
+except:
+	print("Note: this application is meant to be run under uWSGI")
+
+if using_uwsgi:
+	print("Using uWSGI, perfect!")
 
 if __name__ == "__main__":
 	site.application.run()
